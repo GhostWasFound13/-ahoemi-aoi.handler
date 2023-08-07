5cae499c8a6cfd0735617860fc8bd4963cd6b47f
aho-emi <136156874+aho-emi@users.noreply.github.com>
2023-08-07 05:00:02 +0000

    c

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { Handler } = require("./src/handler.js");
 
 console.log('\033[36mInitializing Aoi.Handler\u001b[0m');
 
-
+setTimeout(() => {
+  console.log("Initialized all the handlers")
+}, 1500)
 
 module.exports = {
   Handler
